f70d76efeb855a57eca086f72ccba51f26fb425f
Martin Dvoracek <Dworza@gmail.com>
2025-03-21 17:16:00 +0100

    swagger configured centrally in gateway

--- payment-service/src/main/java/com/dvoracekmartin/paymentservice/config/SecurityConfig.java
+++ payment-service/src/main/java/com/dvoracekmartin/paymentservice/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.dvoracekmartin.inventoryservice.config;
+package com.dvoracekmartin.paymentservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
