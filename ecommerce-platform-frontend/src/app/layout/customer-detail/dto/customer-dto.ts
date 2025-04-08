d5d564e3389560fcea6eae9c76510787d29af5af
Martin Dvoracek <Dworza@gmail.com>
2025-04-08 09:58:09 +0200

    phone added to customer

--- ecommerce-platform-frontend/src/app/layout/customer-detail/dto/customer-dto.ts
+++ ecommerce-platform-frontend/src/app/layout/customer-detail/dto/customer-dto.ts
@@ -2,9 +2,9 @@ import {CustomerBillingAddressDTO} from './custommer-billing-address-dto';
 import {CustomerAddressDTO} from './customer-address-dto';
 
 export class CustomerDTO {
-  id: string;
   firstName: string | null;
   lastName: string | null;
+  phone: string | null;
   address?: CustomerAddressDTO | null;
   billingAddress?: CustomerBillingAddressDTO | null;
 }
