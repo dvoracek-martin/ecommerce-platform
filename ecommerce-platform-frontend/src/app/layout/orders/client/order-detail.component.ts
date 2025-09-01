1c12dcbe6148ae8d1e1f9a6005ebbb6f9e4c50f7
Martin Dvoracek <Dworza@gmail.com>
2025-09-01 18:33:14 +0200

    checkout adjusted

---
+++ ecommerce-platform-frontend/src/app/layout/orders/client/order-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-detail',
+  standalone: false,
+  templateUrl: './order-detail.component.html',
+  styleUrl: './order-detail.component.scss'
+})
+export class OrderDetailComponent {
+
+}
