ed7183af9cc25464699ed000bd7dc8ceca1aa6e2
Martin Dvoracek <Dworza@gmail.com>
2025-09-18 01:54:11 +0200

    big pack of redesign of admin UI

--- common/src/main/java/com/dvoracekmartin/common/dto/category/ResponseCategoryDTO.java
+++ common/src/main/java/com/dvoracekmartin/common/dto/category/ResponseCategoryDTO.java
@@ -4,16 +4,18 @@ import com.dvoracekmartin.common.dto.base.BaseUpdateOrResponseDTO;
 import com.dvoracekmartin.common.dto.media.MediaDTO;
 import com.dvoracekmartin.common.dto.tag.ResponseTagDTO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class ResponseCategoryDTO extends BaseUpdateOrResponseDTO {
 
-    private List<ResponseTagDTO> tagIds;
+    private List<ResponseTagDTO> responseTagDTOS;
 
-    public ResponseCategoryDTO(Long id, String name, String description, int priority, boolean active, List<MediaDTO> media, List<ResponseTagDTO> tagIds) {
+    public ResponseCategoryDTO(Long id, String name, String description, int priority, boolean active, List<MediaDTO> media, List<ResponseTagDTO> responseTagDTOS) {
         super(id, name, description, priority, active, media);
-        this.tagIds = tagIds;
+        this.responseTagDTOS = responseTagDTOS;
     }
 }
