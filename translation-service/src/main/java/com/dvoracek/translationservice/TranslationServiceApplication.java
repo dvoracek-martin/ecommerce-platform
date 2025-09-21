59b66e267b2a12c8f9cc65077ca6bdd50d5c7935
Martin Dvoracek <Dworza@gmail.com>
2025-09-21 02:42:58 +0200

    translation-service created

---
+++ translation-service/src/main/java/com/dvoracek/translationservice/TranslationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.dvoracek.translationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TranslationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationServiceApplication.class, args);
+    }
+
+}
